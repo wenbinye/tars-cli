70d0ea690c9195135e1a54dfa369876b813143e3
Ye Wenbin <wenbinye@gmail.com>
2020-11-11 19:32:00 +0800

    add command to get config by name

--- src/commands/ConfigListCommand.php
+++ src/commands/ConfigListCommand.php
@@ -16,15 +16,18 @@ class ConfigListCommand extends AbstractCommand
         $this->setName('config:list');
         $this->setDescription('Lists config');
         $this->addOption('level', null, InputOption::VALUE_REQUIRED, '配置级别，可选值app|set|area|group|server', 'server');
-        $this->addOption('id', null, InputOption::VALUE_REQUIRED, '查看配置');
+        $this->addOption('id', null, InputOption::VALUE_REQUIRED, '配置ID');
+        $this->addOption('name', null, InputOption::VALUE_REQUIRED, '配置Name');
         $this->addOption('out', null, InputOption::VALUE_REQUIRED, '输出文件');
         $this->addArgument('server', InputArgument::OPTIONAL, 'Server id or name');
     }
 
     protected function handle(): void
     {
-        if ($this->input->getOption('id')) {
-            $this->showConfigById();
+        if ($configId = $this->input->getOption('id')) {
+            $this->showConfigById($configId);
+        } elseif ($configName = $this->input->getOption('name')) {
+            $this->showConfigByName($configName);
         } else {
             $this->showConfigFileList();
         }
@@ -32,20 +35,7 @@ class ConfigListCommand extends AbstractCommand
 
     protected function showConfigFileList(): void
     {
-        $level = ConfigLevel::fromName($this->input->getOption('level'));
-        if (!isset($level)) {
-            throw new \InvalidArgumentException('level 级别不正确');
-        }
-        $serverId = $this->input->getArgument('server');
-        if (!$serverId) {
-            throw new \InvalidArgumentException('server 不能为空');
-        }
-        $serverName = $this->getTarsClient()->getServerName($serverId);
-        $files = $this->getTarsClient()->get('config_file_list', [
-            'application' => $serverName->getApplication(),
-            'server_name' => $serverName->getServerName(),
-            'level' => $level,
-        ]);
+        $files = $this->getConfigFiles();
         $table = $this->createTable(['ID', '服务', '文件名称', '最后修改时间']);
         foreach ($files as $file) {
             $table->addRow([$file['id'], $file['server_name'], $file['filename'], $file['posttime']]);
@@ -53,9 +43,20 @@ class ConfigListCommand extends AbstractCommand
         $table->render();
     }
 
-    private function showConfigById(): void
+    private function showConfigByName($configName): void
     {
-        $configId = $this->input->getOption('id');
+        foreach ($this->getConfigFiles() as $file) {
+            if ($file['filename'] === $configName) {
+                $this->showConfigById($file['id']);
+
+                return;
+            }
+        }
+        throw new \InvalidArgumentException("配置文件 $configName 不存在");
+    }
+
+    private function showConfigById($configId): void
+    {
         $result = $this->getTarsClient()->get('config_file', ['id' => $configId]);
         if (empty($result)) {
             throw new \InvalidArgumentException("配置 $configId 不存在");
@@ -74,5 +75,24 @@ class ConfigListCommand extends AbstractCommand
             $this->output->writeln('文件内容:');
             $this->output->writeln($result['config']);
         }
+    }
+
+    protected function getConfigFiles(): array
+    {
+        $level = ConfigLevel::fromName($this->input->getOption('level'));
+        if (!isset($level)) {
+            throw new \InvalidArgumentException('level 级别不正确');
+        }
+        $serverId = $this->input->getArgument('server');
+        if (!$serverId) {
+            throw new \InvalidArgumentException('server 不能为空');
+        }
+        $serverName = $this->getTarsClient()->getServerName($serverId);
+
+        return $this->getTarsClient()->get('config_file_list', [
+            'application' => $serverName->getApplication(),
+            'server_name' => $serverName->getServerName(),
+            'level' => $level,
+        ]);
     }
 }
